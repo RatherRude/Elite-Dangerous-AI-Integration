19b82e1dc1accc1b88e8e2fcfdfa278508271857
Luca Haneklau <git@luca.lsys.ac>
2024-10-28 12:31:05 +0100

    chore: revert to fasterwhisper

--- src/lib/localSTT.py
+++ src/lib/localSTT.py
@@ -1,47 +1,35 @@
 import io
 import time
-from cached_path import cached_path
-from pywhispercpp.model import Model
+from faster_whisper import WhisperModel
 import samplerate
 import soundfile as sf
 
 stt_models_names = [
-    'ggerganov/whisper.cpp/ggml-base-q5_1.bin',
-    'ggerganov/whisper.cpp/ggml-base.bin',
-    'ggerganov/whisper.cpp/ggml-base.en-q5_1.bin',
-    'ggerganov/whisper.cpp/ggml-base.en.bin',
-    'ggerganov/whisper.cpp/ggml-large-v1.bin',
-    'ggerganov/whisper.cpp/ggml-large-v2-q5_0.bin',
-    'ggerganov/whisper.cpp/ggml-large-v2.bin',
-    'ggerganov/whisper.cpp/ggml-large-v3-q5_0.bin',
-    'ggerganov/whisper.cpp/ggml-large-v3-turbo-q5_0.bin',
-    'ggerganov/whisper.cpp/ggml-large-v3-turbo.bin',
-    'ggerganov/whisper.cpp/ggml-large-v3.bin',
-    'ggerganov/whisper.cpp/ggml-medium-q5_0.bin',
-    'ggerganov/whisper.cpp/ggml-medium.bin',
-    'ggerganov/whisper.cpp/ggml-medium.en-q5_0.bin',
-    'ggerganov/whisper.cpp/ggml-medium.en.bin',
-    'ggerganov/whisper.cpp/ggml-small-q5_1.bin',
-    'ggerganov/whisper.cpp/ggml-small.bin',
-    'ggerganov/whisper.cpp/ggml-small.en-q5_1.bin',
-    'ggerganov/whisper.cpp/ggml-small.en.bin',
-    'ggerganov/whisper.cpp/ggml-tiny-q5_1.bin',
-    'ggerganov/whisper.cpp/ggml-tiny.bin',
-    'ggerganov/whisper.cpp/ggml-tiny.en-q5_1.bin',
-    'ggerganov/whisper.cpp/ggml-tiny.en-q8_0.bin',
-    'ggerganov/whisper.cpp/ggml-tiny.en.bin',
-    'distil-whisper/distil-medium.en/ggml-medium-32-2.en.bin',
-    'distil-whisper/distil-large-v2/ggml-large-32-2.en.bin',
-    'distil-whisper/distil-large-v3-ggml/ggml-distil-large-v3.bin'
+    'distil-medium.en',
+    'distil-small.en', 
+
+    'tiny', 
+    'tiny.en', 
+    'base', 
+    'base.en',
+    'small', 
+    'small.en', 
+    'medium', 
+    'medium.en', 
+    'large-v1',
+    'large-v2', 
+    'large-v3', 
+    'large', 
+    'distil-large-v2',
+    'distil-large-v3',
+    'deepdml/faster-whisper-large-v3-turbo-ct2',
 ]
 
 def init_stt(model_name="distil-medium.en"):
-    file = cached_path("hf://"+model_name).as_posix()
-
-    model = Model(file)
+    model = WhisperModel(model_name, device="cpu", compute_type="int8")
     return model
 
-def stt(model: Model, wav: bytes, language="en-US"):
+def stt(model: WhisperModel, wav: bytes, language="en-US"):
     # convert wav bytes to 16k S16_LE
 
     start = time.time()
@@ -54,14 +42,13 @@ def stt(model: Model, wav: bytes, language="en-US"):
     print("Resample time:", end - start)
 
     start = time.time()
-    segments = model.transcribe(audio)
-    #gen, info = model.transcribe(audio)
-    #
-    #segments = []
-    #for segment in gen:
-    #    segments.append(segment)
+    gen, info = model.transcribe(audio, language=language, beam_size=4)
+    
+    segments = []
+    for segment in gen:
+        segments.append(segment)
     
     end = time.time()
     print("Transcribe time:", end - start)
     
-    return segments, None
+    return segments, info
