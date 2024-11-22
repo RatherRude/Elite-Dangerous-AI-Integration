45d2962182eaab5b2dcbdc7346fd83fb5b8f48d4
Luca Haneklau <git@luca.lsys.ac>
2024-11-22 23:25:20 +0100

    fix: tts tests once more

--- test/lib/test_TTS.py
+++ test/lib/test_TTS.py
@@ -80,7 +80,7 @@ def mock_miniaudio(monkeypatch):
 
 def test_openai_tts_playback(mock_pyaudio, mock_openai):
     """Test OpenAI TTS playback"""
-    tts =  TTS(mock_openai, "tts-1", "nova", 1)
+    tts =  TTS(mock_openai, provider="openai", model="tts-1", voice="nova", speed=1)
     tts.say("Hello world")
     
     while not mock_openai.audio.speech.with_streaming_response.create.called:
@@ -90,7 +90,7 @@ def test_openai_tts_playback(mock_pyaudio, mock_openai):
     
 def test_edge_tts_playback(mock_pyaudio, mock_miniaudio, mock_openai):
     """Test Edge-TTS playback"""
-    tts =  TTS(None, "edge-tts", "edge-tts", "en-GB-SoniaNeural", 1)
+    tts =  TTS(None, provider="edge-tts", model="edge-tts", voice="en-GB-SoniaNeural", speed=1)
     tts.say("Hello world")
     
     while not mock_pyaudio['stream'].write.call_count >= 10:
