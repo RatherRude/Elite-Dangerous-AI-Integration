9cac087589eb5b986c28e4c1657641d7e197ccb6
Luca Haneklau <git@luca.lsys.ac>
2025-01-26 23:54:07 +0100

    feat: global error handling

--- src/Chat.py
+++ src/Chat.py
@@ -297,4 +297,8 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as e:
+        log("error", e, traceback.format_exc())
+        sys.exit(1)
