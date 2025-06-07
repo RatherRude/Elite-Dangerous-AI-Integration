e5fcde8ab99250c9abdcbcd54d3e870ff12c6d6b
Luca Haneklau <git@luca.lsys.ac>
2025-06-07 20:26:11 +0200

    fix: re-add guard against closed std

--- src/lib/Logger.py
+++ src/lib/Logger.py
@@ -26,8 +26,9 @@ def handle_exception(exc_type, exc_value, exc_traceback):
         # Call default handler for keyboard interrupt
         sys.__excepthook__(exc_type, exc_value, exc_traceback)
         return
-        
-    logger.error("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
+    
+    if not sys.stderr.closed:
+        logger.error("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
 
 sys.excepthook = handle_exception
 
