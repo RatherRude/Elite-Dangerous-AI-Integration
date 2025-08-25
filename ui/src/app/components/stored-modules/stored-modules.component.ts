05bd059d39804835adbe31fac95afc7456cce1db
Rather Rude <tremendouslyrude@yandex.com>
2025-08-25 23:54:24 +0200

    feat: blueprints

--- ui/src/app/components/stored-modules/stored-modules.component.ts
+++ ui/src/app/components/stored-modules/stored-modules.component.ts
@@ -103,7 +103,7 @@ export class StoredModulesComponent implements OnInit, OnDestroy {
   }
 
   formatTransferTime(seconds: number): string {
-    if (!seconds) return 'Unknown';
+    if (!seconds) return '-';
     const hours = Math.floor(seconds / 3600);
     const minutes = Math.floor((seconds % 3600) / 60);
     return hours > 0 ? `${hours}h ${minutes}m` : `${minutes}m`;
