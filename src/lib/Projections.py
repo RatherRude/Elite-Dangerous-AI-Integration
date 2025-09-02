024d2e7780a7b4baf2b4ab4182c9a8a890d5cef5
DedHapp3n <155900552+DedHapp3n@users.noreply.github.com>
2025-09-03 01:54:24 +0200

    fuel_calc+inject_fsd

--- src/lib/Projections.py
+++ src/lib/Projections.py
@@ -555,6 +555,7 @@ ShipInfoState = TypedDict('ShipInfoState', {
     "FuelMainCapacity": float,
     "FuelReservoir": float,
     "FuelReservoirCapacity": float,
+    "FSDSynthesis":float,
     "ReportedMaximumJumpRange": float,
     "DriveOptimalMass":float,
     "DriveLinearConst":float,
@@ -588,6 +589,7 @@ class ShipInfo(Projection[ShipInfoState]):
             "FuelReservoir": 0,
             "FuelReservoirCapacity": 0,
             "ReportedMaximumJumpRange": 0,
+            "FSDSynthesis":0,
             "DriveOptimalMass": 0,
             "DriveLinearConst":0,
             "GuardianfsdBooster":0,
@@ -711,8 +713,21 @@ class ShipInfo(Projection[ShipInfoState]):
             fsd_star_boost = event.content.get('BoostValue', 1)
             self.state['JetConeBoost'] = fsd_star_boost
         
+        if isinstance(event, GameEvent) and event.content.get('event') == 'Synthesis':
+            fsd_inject_boost_name = event.content.get('Name', "")
+            
+            if fsd_inject_boost_name == "FSD Basic":
+                self.state['FSDSynthesis'] = 0.25
+                
+            elif fsd_inject_boost_name == "FSD Standard":
+                self.state['FSDSynthesis'] = 0.5
+                
+            elif fsd_inject_boost_name == "FSD Premium":
+                self.state['FSDSynthesis'] = 1
+                  
         if isinstance(event,GameEvent) and event.content.get('event') == 'FSDJump':
             self.state['JetConeBoost'] = 1
+            self.state['FSDSynthesis'] = 0
         
         
         if isinstance(event, GameEvent) and event.content.get('event') == 'Cargo':
@@ -855,7 +870,7 @@ class ShipInfo(Projection[ShipInfoState]):
         drive_max_fuel  = self.state.get("DriveMaxFuel")
         fsd_star_boost = self.state.get("JetConeBoost")
         fsd_boost = self.state.get("GuardianfsdBooster")
-        fsd_inject = 0 # +inject juice 25% , 50% ,100% but cant be with star_boost
+        fsd_inject = self.state.get("FSDSynthesis") # +inject juice 25% , 50% ,100% but cant be with star_boost
 
         if not (unladen_mass > 0 and fuel_capacity > 0 and maximum_jump_range > 0 and drive_max_fuel):
             return 0, 0, 0
@@ -863,6 +878,11 @@ class ShipInfo(Projection[ShipInfoState]):
         current_cargo = self.state.get("ShipCargo")
         current_fuel  = self.state.get("FuelMain")
         current_fuel_reservoir = self.state.get("FuelReservoir")
+        
+        if drive_max_fuel > current_fuel:
+            math_fuel = current_fuel
+        else:
+            math_fuel = drive_max_fuel
 
         minimal_mass = unladen_mass + drive_max_fuel  #max jump with just right anmount
         current_mass = unladen_mass + current_cargo + current_fuel + current_fuel_reservoir  #current mass
@@ -871,11 +891,11 @@ class ShipInfo(Projection[ShipInfoState]):
         log('info', 'current_mass', current_mass)
         log('info', 'maximal_mass', maximal_mass)
         
-        base = lambda M: (drive_optimal_mass / M) * ( (10**3 * drive_max_fuel) / drive_linear_const )**(1/drive_power_const)
+        base = lambda M, F: (drive_optimal_mass / M) * ((10**3 * F) / drive_linear_const )**(1/drive_power_const)
         # adding stuff here for more future fsd boost stuff 
-        min_ly = (base(maximal_mass) + fsd_boost) * fsd_star_boost
-        cur_ly = (base(current_mass) + fsd_boost) * fsd_star_boost
-        max_ly = (base(minimal_mass) + fsd_boost) * fsd_star_boost
+        min_ly = (base(maximal_mass, drive_max_fuel) + fsd_boost) * (fsd_star_boost +fsd_inject)
+        cur_ly = (base(current_mass, math_fuel)       + fsd_boost) * (fsd_star_boost +fsd_inject)
+        max_ly = (base(minimal_mass, drive_max_fuel) + fsd_boost) * (fsd_star_boost +fsd_inject)
         
         return min_ly, cur_ly, max_ly
 
